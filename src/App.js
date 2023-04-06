ebb03deccd1fd0bbb00cc887469c9db8028bdb5b
arixol <arixol@gmail.com>
2023-04-06 00:31:32 -0400

    Added error modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './Components/Users/AddUser';
-
+import UsersList from './Components/Users/UsersList';
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    // When state update relies on the previous state, use function form so
+    // the function automatically gets the previous state as an arg
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: userName, age: userAge },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,22 +2,31 @@ import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault(); // prevent form submission
     if (!enteredUsername.trim().length || !enteredAge.trim().length) {
+      setError({
+        title: 'Invalid User',
+        message: 'Please enter a valid name.',
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Please enter a valid age.',
+      });
       return;
     }
-    console.log('enteredUsername: ', enteredUsername);
-    console.log('enteredAge: ', enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredAge(''); // must also map value to element -- value={enteredUsername}
     setEnteredUsername(''); // must also map value to element -- value={enteredAge}
   };
@@ -30,26 +39,39 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const clearErrorHandler = (event) => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangedHandler}
-          value={enteredUsername}
+    <div>
+      {error && error.hasOwnProperty('title') && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onClick={clearErrorHandler}
         />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangedHandler}
-          value={enteredAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangedHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangedHandler}
+            value={enteredAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
